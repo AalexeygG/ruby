e2af0b34a10a4b409135856852e44d251f90f27c
AalexeygG <110333142+AalexeygG@users.noreply.github.com>
2022-09-08 20:18:51 +0300

    Add files via upload

---
+++ ruby_3.rb
@@ -0,0 +1,16 @@
+#78 page
+class Dog
+	def talk
+		puts "Bark!"
+	end
+	
+	def move(destination)
+		puts "Running to the #{destination}."
+	end
+end
+
+fido = Dog.new
+rex = Dog.new
+
+fido.talk
+rex.move("home")
