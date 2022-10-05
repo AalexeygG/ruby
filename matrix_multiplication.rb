efcb54e54fbd91464626ddef2dee3cdaa687e5ec
AalexeygG <110333142+AalexeygG@users.noreply.github.com>
2022-10-05 16:46:50 +0300

    Add files via upload

--- matrix_multiplication.rb
+++ matrix_multiplication.rb
@@ -1,44 +1,74 @@
-class matrix_maker(m, n)
-	matrix = Array.new(m) { Array.new(n)}
-	for i 0..(m - 1)
-		row = gets().scan(/\d+/)
-		for j 0..(n - 1)
-			matrix_1[i][j] = row[j].to_i
+def matrix_multiplication(matrix_1, matrix_2)
+	m1 = matrix_1.length #кол-во строк в 1-ой
+	n1 = matrix_1[0].length #кол-во столбцов в 1-ой 
+	m2 = n1 #строки во 2-ой
+	n2 = matrix_2[0].length #столбы во 2-ой
+	matrix_mult = []
+	matrix_result = []
+	s = 0
+	(0..m1 - 1).each do |i|
+		(0..n2 - 1).each do |j|
+			(0..m2 - 1).each do |k|
+				s += matrix_1[i][k] * matrix_2[k][j]
+			end
+			matrix_mult.push(s)
+			s = 0
 		end
+		matrix_result.push(matrix_mult)
+		matrix_mult = []
 	end
 	
-	return matrix
+	return matrix_result
+	
 end
+
+def check(m, n)
+	if m <= 0 || n <= 0
+		abort "Try again, bro"
+	end
+end
+
+
 
 puts "Введите размер 1-ой матрицы(строки)"
 m_1 = gets.to_i
 puts "Введите размер 1-ой матрицы(столбцы)"
 n_1 = gets.to_i
 
-if m_1 <= || n_1 <= 0
-	abort "Try again, bro"
-end
+check(m_1, n_1)
 
+matrix_1 = Array.new(m_1) { Array.new(n_1)}
 
-/matrix_1 = Array.new(m_1) { Array.new(n_1)}
-for i 0..(m_1 - 1)
+puts "Введите вашу 1-ую матрицу"
+for i in 0..m_1 - 1
 	row = gets().scan(/\d+/)
-	for j 0..(n_1 - 1)
+	for j in 0..n_1 - 1
 		matrix_1[i][j] = row[j].to_i
 	end
-end/
+end
 
-matrix_1 = matrix_maker.new(m_1, n_1)
 
 puts "Введите размер 2-ой матрицы(строки)"
 m_2 = gets.to_i
 puts "Введите размер 2-ой матрицы(столбцы)"
 n_2 = gets.to_i
 
-matrix_2 = matrix_maker.new(m_2, n_2)
+check(m_2, n_2)
 
+matrix_2 = Array.new(m_2) { Array.new(n_2)}
 
+puts "Введите вашу 2-ую матрицу"
 if matrix_1[0].length == matrix_2.length
-	matrix_result = Array.new(m_1) {Array.new(n_2)}
-	for i 0..m_1 - 1
-		
+	(0..m_2 - 1).each do |i|
+		row = gets().scan(/\d+/)
+		(0..n_2 - 1).each do |j|
+			matrix_2[i][j] = row[j].to_i
+		end
+	end
+	
+	puts matrix_multiplication(matrix_1, matrix_2)
+	
+	
+else
+	puts "Multiplication is unreal(column_1 != row_2)"
+end
